a5bc6f06ffdc056eac6703ff70c835441c725a70
GuilhermeGetEdu <guilherme@getedu.com.br>
2026-08-19 18:50:24 -0300

    Fix document URLs in ARQUIVOS_DRIVE
    Updated document links for 'Minha Casa' and 'Meu Barco'.

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ st.title("🎙️ Motor de Avaliação de Leitura (MVP)")
 WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbwaBC2f1R10P3RSxJVoazh7nTKT2NzA4Goz-abmjml0S81g1wpYExry4ic_WhKfzI0d/exec"
 
 ARQUIVOS_DRIVE = {
-    "Minha Casa": "https://docs.google.com/document/d/177yW8EjrHvlIEc3kQBuulYCxb6bpcquc7t8DbNVZY4/edit",
-    "Meu Barco": "https://docs.google.com/document/d/1rzo5imfn3IzQ_dkIDOQi2ScEATn1aPIKd6usnrk6r0w/edit"
+    "Minha Casa": "https://docs.google.com/document/d/177yW8EjrHvllEc3kQBuulYCxvb6bpcquc7t8DbNVZY4/edit",
+    "Meu Barco": "https://docs.google.com/document/d/1rzo5imfn3IzQ_dkIDOQi2ScEATn1aPIKd6usnrk6rOw/edit"
 }
 
 # Dados do Estudante
